2eb01b365393241aa4e7f481d26d51f1f6700781
jujumilk3 <jujumilk3@gmail.com>
2025-03-07 20:28:17 +0900

    Enhance middleware example with additional middleware and logging

--- examples/middleware_example.py
+++ examples/middleware_example.py
@@ -2,6 +2,8 @@
 An example of using middleware with ezmcp.
 """
 
+import asyncio
+import random
 import time
 
 from starlette.requests import Request
@@ -15,6 +17,7 @@ app = ezmcp("middleware-example", debug=True)
 # Define a middleware using the decorator
 @app.middleware
 async def process_time_middleware(request: Request, call_next):
+    print(1)
     print("process_time_middleware called")
     """Add a header with the processing time."""
     start_time = time.perf_counter()
@@ -22,7 +25,6 @@ async def process_time_middleware(request: Request, call_next):
     process_time = time.perf_counter() - start_time
     response.headers["X-Process-Time"] = str(process_time)
     print(f"process_time: {process_time}")
-    print(f"response: {response}")
     return response
 
 
@@ -31,14 +33,35 @@ class CustomHeaderMiddleware(EzmcpHTTPMiddleware):
     """Add a custom header to the response."""
 
     async def dispatch(self, request: Request, call_next):
+        print(2)
+        print("CustomHeaderMiddleware called")
         """Add a custom header to the response."""
         response = await call_next(request)
         response.headers["X-Custom-Header"] = "Hello from middleware!"
         return response
 
 
+class CustomSleepMiddleware(EzmcpHTTPMiddleware):
+    """Sleep for a random amount of time."""
+
+    async def dispatch(self, request: Request, call_next):
+        print(3)
+        print("CustomSleepMiddleware called")
+        """Sleep for a random amount of time."""
+        await asyncio.sleep(random.randint(0, 10))
+        return await call_next(request)
+
+
 # Add the custom middleware
 app.add_middleware(CustomHeaderMiddleware)
+app.add_middleware(CustomSleepMiddleware)
+
+
+@app.middleware
+async def custom_middleware(request: Request, call_next):
+    print(4)
+    print("custom_middleware called")
+    return await call_next(request)
 
 
 # Define a tool
